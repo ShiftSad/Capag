437adc6dddf4021574f032a0463dc24632b4cb3b
Sadness <rosalimavictor@gmail.com>
2025-06-18 14:37:35 -0300

    First commit

---
+++ src/main/java/codes/shiftmc/capag/CapagApplication.java
@@ -0,0 +1,13 @@
+package codes.shiftmc.capag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapagApplication.class, args);
+    }
+
+}
---
+++ src/test/java/codes/shiftmc/capag/CapagApplicationTests.java
@@ -0,0 +1,13 @@
+package codes.shiftmc.capag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
